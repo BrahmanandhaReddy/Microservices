d24d47776aea0f2daf3a382ce8430e4cb7ded30f
Brahmanandhareddy <mbnrswathi143@gmail.com>
2020-03-17 16:53:56 +0530

    remove userservice and ordersercive

--- UserService/src/main/java/com/srit/user/exception/EntityEmptyException.java
+++
@@ -1,9 +0,0 @@
-package com.srit.user.exception;
-
-public class EntityEmptyException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-
-	public EntityEmptyException (String exception) {
-		super(exception);
-	}
-}
